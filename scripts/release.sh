0a2fef0fc5a1766f95cbae78b7797f7266d00688
Jongsu Liam Kim <jongsukim8@gmail.com>
2024-12-11 06:18:42 +0900

    fix

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,6 +3,7 @@ echo "Release script"
 echo "Next version: $(uv run git-cliff --bumped-version)"
 uv run git-cliff --strip header -o CHANGELOG.md
 uv run git-cliff --latest --strip header -o RELEASE.md
+git add CHANGELOG.md RELEASE.md
 git commit -am "docs: Add CHANGELOG.md and RELEASE.md to release $(uv run git-cliff --bumped-version)"
-git tag -a $(uv run git-cliff --bumped-version)
+git tag -a $(uv run git-cliff --bumped-version) -m "Release $(uv run git-cliff --bumped-version)"
 git push origin tag $(uv run git-cliff --bumped-version)
