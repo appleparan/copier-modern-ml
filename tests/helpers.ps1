90b96aa48cb5baa887e17cb758b2e8ad03c331e0
Jongsu Liam Kim <jongsukim8@gmail.com>
2025-02-16 04:17:33 +0900

    fix: Add answers for test

--- tests/helpers.ps1
+++ tests/helpers.ps1
@@ -8,11 +8,13 @@ function Generate {
         -d project_name="Modern Python Machine Learning Workflow Package" `
         -d project_slug="modern_ml_template_testing" `
         -d project_description="Testing template" `
+        -d repo_url="https://example.com" `
         -d version="0.1.0" `
         -d author_fullname="Author123" `
         -d author_email="username123@user.emailaddress" `
         -d copyright_license="MIT License" `
         -d package="yes" `
-        -d native-tls="no" `
+        -d native_tls="no" `
+        -d allow_insecure_tls="no" `
         -d ci="github"
 }
